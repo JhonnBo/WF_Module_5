9fe28cb86121ccce7f2992fcc78b189942bb892a
JhonnBo <jhonnbo@gmail.com>
2023-05-05 15:01:04 +0300

    Add project files.

---
+++ NoModal/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NoModal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Form2 ChildForm { get; set; }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ChildForm != null)
+            {
+                ChildForm.Activate();
+                return;
+            }
+            ChildForm = new Form2();
+            //ChildForm.ShowInTaskbar = false; // не отображать окно на панели задач
+            ChildForm.MainForm = this;
+            ChildForm.Show();
+        }
+    }
+}
